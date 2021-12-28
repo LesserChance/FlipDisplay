0896397dabdf0950cb312b4cfd908060ff5d4d7b
Ryan Bateman <rbateman@etsy.com>
2021-12-28 15:45:17 -0500

    FlipDisplay documentation

--- src/FlipDisplay/FlipDisplay.cpp
+++ src/FlipDisplay/FlipDisplay.cpp
@@ -2,6 +2,10 @@
 
 #include "FlipDisplay.h"
 
+/*************************************
+ * CONSTRUCTOR
+ *************************************/
+
 FlipDisplay::FlipDisplay() {
     disable(true);
     for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
@@ -10,24 +14,28 @@ FlipDisplay::FlipDisplay() {
 
     _setAnimationType = AnimationType::ARRIVE_TOGETHER;
     _scrollAnimationType = AnimationType::CASCADE_RIGHT;
-    
+
     _type = _setAnimationType;
 }
+
+/*************************************
+ * PUBLIC
+ *************************************/
 
 void FlipDisplay::home() {
     if (_isHoming) {
 #if DEBUG_LOOP
-    Serial.println("HOMING ALREADY IN PROGRESS, NOT STARTNG");
+        Serial.println("HOMING ALREADY IN PROGRESS, NOT STARTNG");
 #endif
         return;
     }
-    
+
 #if DEBUG_LOOP
     Serial.println("HOMING START");
 #endif
     _isHoming = true;
     readInButtonRegister();
-    
+
     for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
         uint8_t buttonState = !bitRead(_buttonRegisterInput, i);
         _characters[i]->setButtonState(buttonState);
@@ -38,44 +46,85 @@ void FlipDisplay::home() {
 void FlipDisplay::run() {
     _currentTime = micros();
 
-    bool isHoming = false;
-    int motorOffCount = 0;
-
+    // check for any state changes we want to do before running
     checkForScroll();
     checkForEnable();
 
     int pauseCount = 0;
     for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-        FlipDisplayCharacter::FlipDisplayCharacterState characterState = _characters[i]->run();
-
-        switch (characterState) {
-            case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_PAST_BUTTON:
-            case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_TO_BUTTON:
-            case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_TO_OFFSET:
-                // true as long as at least one character is homing
-                isHoming = true;
-                break;
-
-            case FlipDisplayCharacter::FlipDisplayCharacterState::MOTOR_OFF:
-                motorOffCount++;
-                break;
-
-            default:
-                break;
-        }
+        FlipDisplayCharacter::FlipDisplayCharacterState characterState =
+            _characters[i]->run();
     }
 
-    if (_isHoming && !isHoming) {
-        _isHoming = isHoming;
-#if DEBUG_LOOP
-        Serial.println("HOMING COMPLETE");
+    // check for any state changes we want to do after running
+    checkForHoming();
+    checkForDisable();
+
+    // write all the changes to the GPIO
+    updateRegisters();
+}
+
+void FlipDisplay::setDisplay(String displayString) {
+    displayString.toUpperCase();
+    _currentDisplay = displayString;
+    _currentDisplayScrollPosition = 0;
+
+#if DEBUG
+    Serial.print("SETTING DISPLAY TO: ");
+    Serial.println(_currentDisplay);
+#endif
+
+    setAnimationType(_setAnimationType);
+    lerpToCurrentDisplay();
+}
+
+int FlipDisplay::stepCharacter(int characterIndex) {
+    _characters[characterIndex]->step(1);
+    return _characters[characterIndex]->getCurrentOffsetFromButton() + 1;
+}
+
+void FlipDisplay::setAnimationType(AnimationType type) { _type = type; }
+
+void FlipDisplay::enable(bool force) {
+    if (force) {
+        digitalWrite(enablePin, LOW);
+        return;
+    }
+
+    if (_isDisabled) {
+        digitalWrite(enablePin, LOW);
+        _isDisabled = false;
+#if DEBUG
+        Serial.println("ENABLE ALL MOTORS");
 #endif
     }
+}
 
-    if (!_isDisabled && motorOffCount == CHARACTER_COUNT) {
-        disable();
+void FlipDisplay::disable(bool force) {
+    if (force) {
+        digitalWrite(enablePin, HIGH);
+        return;
     }
-    updateRegisters();
+
+    if (!_isDisabled) {
+        digitalWrite(enablePin, HIGH);
+        _isDisabled = true;
+#if DEBUG
+        Serial.println("DISABLE ALL MOTORS");
+#endif
+    }
+}
+
+/*************************************
+ * PRIVATE
+ *************************************/
+
+void FlipDisplay::readInButtonRegister() {
+    _buttonRegisterInput = 0b0000000;
+
+    for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
+        bitWrite(_buttonRegisterInput, i, digitalRead(LOOP_PIN[i]));
+    }
 }
 
 void FlipDisplay::updateRegisters() {
@@ -86,75 +135,12 @@ void FlipDisplay::updateRegisters() {
         for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
             digitalWrite(STEP_PIN[i], bitRead(stepPinRegisterOutput, i));
         }
-        
+
         readInButtonRegister();
-        
+
         for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
             uint8_t buttonState = !bitRead(_buttonRegisterInput, i);
             _characters[i]->debounceButtonState(buttonState);
-        }
-    }
-}
-
-void FlipDisplay::readInButtonRegister() {
-    _buttonRegisterInput = 0b0000000;
-
-    for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-        bitWrite(_buttonRegisterInput, i, digitalRead(LOOP_PIN[i]));
-    }
-}
-
-void FlipDisplay::checkForScroll() {
-    if (SCROLLING_ENABLED && _nextScrollTime != 0 && _currentTime > _nextScrollTime) {
-        // if any motors are still moving, do not scroll
-        for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-            switch (_characters[i]->getState()) {
-                case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_PAST_BUTTON:
-                case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_TO_BUTTON:
-                case FlipDisplayCharacter::FlipDisplayCharacterState::HOMING_TO_OFFSET:
-                case FlipDisplayCharacter::FlipDisplayCharacterState::RUNNING:
-                    //dont scroll right now
-                    _nextScrollTime += WAIT_TO_SCROLL;
-                    return;
-            }
-        }
-
-        // all motors are paused - we're ok to scroll now
-        _currentDisplayScrollPosition += CHARS_TO_SCROLL;
-
-#if DEBUG
-    Serial.print("SCROLLING TO POSITION: ");
-    Serial.print(_currentDisplayScrollPosition);
-
-    String displayString = _currentDisplay.substring(_currentDisplayScrollPosition, _currentDisplayScrollPosition + CHARACTER_COUNT);
-    Serial.print(" (");
-    Serial.print(displayString);
-    Serial.println(")");
-#endif
-
-        setAnimationType(_scrollAnimationType);
-        lerpToCurrentDisplay();
-    }
-}
-
-void FlipDisplay::checkForEnable() {
-    if (_isDisabled) {
-        bool allMotorsDisabled = true;
-
-        for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-            switch (_characters[i]->getState()) {
-                case FlipDisplayCharacter::FlipDisplayCharacterState::MOTOR_OFF:
-                    break;
-
-                default:
-                    allMotorsDisabled = false;
-                    break;
-            }
-        }
-        
-        if (!allMotorsDisabled) {
-            // need to renable the motors
-            enable();
         }
     }
 }
@@ -168,55 +154,111 @@ byte FlipDisplay::getRegisterOutput() {
     return stepPinRegisterOutput;
 }
 
-void FlipDisplay::setDisplay(String displayString) {
-    displayString.toUpperCase();
-    _currentDisplay = displayString;
-    _currentDisplayScrollPosition = 0;
-    
+void FlipDisplay::checkForScroll() {
+    if (SCROLLING_ENABLED && _nextScrollTime != 0 &&
+        _currentTime > _nextScrollTime) {
+        // if any motors are still moving, do not scroll
+        for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
+            switch (_characters[i]->getState()) {
+                case FlipDisplayCharacter::FlipDisplayCharacterState::
+                    HOMING_PAST_BUTTON:
+                case FlipDisplayCharacter::FlipDisplayCharacterState::
+                    HOMING_TO_BUTTON:
+                case FlipDisplayCharacter::FlipDisplayCharacterState::
+                    HOMING_TO_OFFSET:
+                case FlipDisplayCharacter::FlipDisplayCharacterState::RUNNING:
+                    // dont scroll right now
+                    _nextScrollTime += WAIT_TO_SCROLL;
+                    return;
+            }
+        }
+
+        // all motors are paused - we're ok to scroll now
+        _currentDisplayScrollPosition += CHARS_TO_SCROLL;
+
 #if DEBUG
-    Serial.print("SETTING DISPLAY TO: ");
-    Serial.println(_currentDisplay);
+        Serial.print("SCROLLING TO POSITION: ");
+        Serial.print(_currentDisplayScrollPosition);
+
+        String displayString = _currentDisplay.substring(
+            _currentDisplayScrollPosition,
+            _currentDisplayScrollPosition + CHARACTER_COUNT);
+        Serial.print(" (");
+        Serial.print(displayString);
+        Serial.println(")");
 #endif
 
-    setAnimationType(_setAnimationType);
-    lerpToCurrentDisplay();
+        setAnimationType(_scrollAnimationType);
+        lerpToCurrentDisplay();
+    }
 }
 
-void FlipDisplay::setAnimationType(AnimationType type) {
-    _type = type;
-}
+void FlipDisplay::checkForHoming() {
+    bool isHoming = false;
 
-void FlipDisplay::enable(bool force) {
-    if (_isDisabled || force) {
-        digitalWrite(enablePin, LOW);
-        _isDisabled = false;
-#if DEBUG
-    Serial.println("ENABLE ALL MOTORS");
+    for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
+        switch (_characters[i]->getState()) {
+            case FlipDisplayCharacter::FlipDisplayCharacterState::
+                HOMING_PAST_BUTTON:
+            case FlipDisplayCharacter::FlipDisplayCharacterState::
+                HOMING_TO_BUTTON:
+            case FlipDisplayCharacter::FlipDisplayCharacterState::
+                HOMING_TO_OFFSET:
+                // true as long as at least one character is homing
+                isHoming = true;
+                break;
+        }
+    }
+
+    if (_isHoming && !isHoming) {
+        _isHoming = isHoming;
+#if DEBUG_LOOP
+        Serial.println("HOMING COMPLETE");
 #endif
     }
 }
 
-void FlipDisplay::disable(bool force) {
-    if (!_isDisabled || force) {
-        digitalWrite(enablePin, HIGH);
-        _isDisabled = true;
-#if DEBUG
-    Serial.println("DISABLE ALL MOTORS");
-#endif
+void FlipDisplay::checkForEnable() {
+    if (_isDisabled && !areAllMotorsDisabled()) {
+        // need to renable the motors
+        enable();
     }
 }
 
-int FlipDisplay::stepCharacter(int characterIndex) {
-    _characters[characterIndex]->step(1);
-    return _characters[characterIndex]->getCurrentOffsetFromButton() + 1;
+void FlipDisplay::checkForDisable() {
+    if (!_isDisabled && areAllMotorsDisabled()) {
+        // need to disable the motors
+        disable();
+    }
+}
+
+bool FlipDisplay::areAllMotorsDisabled() {
+    bool allMotorsDisabled = true;
+
+    for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
+        switch (_characters[i]->getState()) {
+            case FlipDisplayCharacter::FlipDisplayCharacterState::MOTOR_OFF:
+                break;
+
+            default:
+                allMotorsDisabled = false;
+                break;
+        }
+    }
+
+    return allMotorsDisabled;
 }
 
 void FlipDisplay::lerpToCurrentDisplay() {
-    String displayString = _currentDisplay.substring(_currentDisplayScrollPosition, _currentDisplayScrollPosition + CHARACTER_COUNT);
+    String displayString = _currentDisplay.substring(
+        _currentDisplayScrollPosition,
+        _currentDisplayScrollPosition + CHARACTER_COUNT);
     FlipDisplayLerp lerps[CHARACTER_COUNT];
 
+    // determine the number of steps for each character
     for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-        char displayChar = (displayString.length() > i) ? displayString[i] : ' ';
+        char displayChar =
+            (displayString.length() > i) ? displayString[i] : ' ';
         int steps = _characters[i]->getStepsToChar(displayChar);
 
         if (steps > 0 && steps < MIN_STEPS_PER_LERP) {
@@ -230,11 +272,12 @@ void FlipDisplay::lerpToCurrentDisplay() {
             _characters[i]->allowLoop();
         }
     }
-    
-    unsigned long maxDuration = FlipDisplayLerp::getMaxDuration(lerps, CHARACTER_COUNT);
 
-    // mutate the lerps based on the animation type
-    switch(_type) {
+    // mutate the lerps duration and delay based on the animation type
+    unsigned long maxDuration =
+        FlipDisplayLerp::getMaxDuration(lerps, CHARACTER_COUNT);
+
+    switch (_type) {
         case AnimationType::ARRIVE_TOGETHER:
             for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
                 lerps[i].setDelay(maxDuration - lerps[i].getTotalDuration());
@@ -247,46 +290,46 @@ void FlipDisplay::lerpToCurrentDisplay() {
             }
             break;
 
-        case AnimationType::CASCADE_LEFT:
-            {
-                int delayAmt = 0;
-                for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
-                    lerps[i].setDuration(maxDuration);
+        case AnimationType::CASCADE_LEFT: {
+            int delayAmt = 0;
+            for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
+                lerps[i].setDuration(maxDuration);
 
-                    if (lerps[i].getTotalSteps() > 0) {
-                        lerps[i].setDelay(delayAmt +  maxDuration - lerps[i].getTotalDuration());
-                        delayAmt += (maxDuration / 2);
-                    }
+                if (lerps[i].getTotalSteps() > 0) {
+                    lerps[i].setDelay(delayAmt + maxDuration -
+                                      lerps[i].getTotalDuration());
+                    delayAmt += (maxDuration / 2);
                 }
             }
-            break;
+        } break;
 
-        case AnimationType::CASCADE_RIGHT:
-            {
-                int delayAmt = 0;
-                for (int i = CHARACTER_COUNT - 1; i >= START_CHARACTER; i--) {
-                    lerps[i].setDuration(maxDuration);
+        case AnimationType::CASCADE_RIGHT: {
+            int delayAmt = 0;
+            for (int i = CHARACTER_COUNT - 1; i >= START_CHARACTER; i--) {
+                lerps[i].setDuration(maxDuration);
 
-                    if (lerps[i].getTotalSteps() > 0) {
-                        lerps[i].setDelay(delayAmt +  maxDuration - lerps[i].getTotalDuration());
-                        delayAmt += (maxDuration / 2);
-                    }
+                if (lerps[i].getTotalSteps() > 0) {
+                    lerps[i].setDelay(delayAmt + maxDuration -
+                                      lerps[i].getTotalDuration());
+                    delayAmt += (maxDuration / 2);
                 }
             }
-            break;
+        } break;
 
         case AnimationType::START_TOGETHER:
         default:
             break;
     }
-    
+
+    // start the lerps
     for (int i = START_CHARACTER; i < CHARACTER_COUNT; i++) {
         _characters[i]->startLerp(lerps[i]);
     }
 
     if (SCROLLING_ENABLED) {
         // Determine if we need to scroll
-        if (_currentDisplay.length() > _currentDisplayScrollPosition + CHARACTER_COUNT){
+        if (_currentDisplay.length() >
+            _currentDisplayScrollPosition + CHARACTER_COUNT) {
             _nextScrollTime = _currentTime + maxDuration + WAIT_TO_SCROLL;
         } else {
             _nextScrollTime = 0;
--- src/FlipDisplay/FlipDisplay.h
+++ src/FlipDisplay/FlipDisplay.h
@@ -10,53 +10,166 @@
 #include "FlipDisplayLerp.h"
 
 class FlipDisplay {
-    public:
-        typedef enum {
-            START_TOGETHER = 0,             // all characters start at the same time
-            ARRIVE_TOGETHER = 1,            // all characters arrive at the destination at the same time
-            SAME_DURATION = 2,              // all characters turn for the same duration (variable speed)
+   public:
+    typedef enum {
+        // all characters start at the same time
+        START_TOGETHER = 0,
+        // all characters arrive at the destination at the same time
+        ARRIVE_TOGETHER = 1,
+        // all characters turn for the same duration (variable speed)
+        SAME_DURATION = 2,
+        // characters cascade form left to right
+        CASCADE_LEFT = 3,
+        // characters cascade form right to left
+        CASCADE_RIGHT = 4
+    } AnimationType;
 
-            CASCADE_LEFT = 3,
-            CASCADE_RIGHT = 4              
-        } AnimationType;
+    FlipDisplay();
 
-        FlipDisplay();
-        
-        void home();
-        void run();
-        byte getRegisterOutput();
-        int stepCharacter(int characterIndex);
-        void setDisplay(String display);
-        void setAnimationType(AnimationType type);
+    /**
+     * @brief Re-home the entire display
+     * Using the buttons on each character, go past the button if on it, then to
+     * the button, then to the starting offset this gets each character to a
+     * zero position
+     */
+    void home();
 
-        void enable(bool force = false);
-        void disable(bool force = false);
+    /**
+     * @brief the main loop function, runs all characters
+     * Run each character's loop function and iven the current state of the
+     * display, determine if we need to scroll, enable, or disable
+     */
+    void run();
 
-    private:
-        AnimationType _type;
-        AnimationType _setAnimationType;
-        AnimationType _scrollAnimationType;
+    /**
+     * @brief set the display
+     * Switch the animation type to the "set" type, and determine all the lerps
+     * necessary to get to the new value
+     *
+     * @param displayString the new string to set the display to
+     */
+    void setDisplay(String display);
 
-        String _currentDisplay;
+    /**
+     * @brief step an inidivial character once
+     * @param characterIndex which character to step
+     * @return int the new offset of the character
+     */
+    int stepCharacter(int characterIndex);
 
-        FlipDisplayCharacter * _characters[CHARACTER_COUNT];
-        unsigned long _currentTime;
-        
-        int _currentDisplayScrollPosition;
-        unsigned long _nextScrollTime;
+    /**
+     * @brief the next time the display changes it'll use this animation type
+     * @param type the type of animation
+     */
+    void setAnimationType(AnimationType type);
 
-        bool _isHoming = false;
-        bool _isDisabled = false;
-        
-        byte _lastStepPinRegisterOutput = 0b00000000;
-        byte _buttonRegisterInput = 0b00000000;
+    /**
+     * @brief enable the motors
+     * @param force if true, the GPIO is updated, but the state is not, this can
+     * be used for debugging
+     */
+    void enable(bool force = false);
 
-        void readInButtonRegister();
-        void updateRegisters();
+    /**
+     * @brief disable the motors
+     * @param force if true, the GPIO is updated, but the state is not, this can
+     * be used for debugging
+     */
+    void disable(bool force = false);
 
-        void checkForScroll();
-        void checkForEnable();
-        void lerpToCurrentDisplay();
+   private:
+    /*************************************
+     * PROPERTIES
+     *************************************/
+    // the current animation type
+    AnimationType _type;
+
+    // the animation type used when setting a new string
+    AnimationType _setAnimationType;
+
+    // the animation type used when scrolling
+    AnimationType _scrollAnimationType;
+
+    // the full string that is currently displayed (or in progress of being
+    // displayed)
+    String _currentDisplay;
+
+    // the character objects the display is controlling
+    FlipDisplayCharacter* _characters[CHARACTER_COUNT];
+
+    // the timestamp of the most recent run call
+    unsigned long _currentTime;
+
+    // the index of the first character in _currentDisplay to display
+    int _currentDisplayScrollPosition;
+
+    // the next time we should check to see if the display should scroll
+    unsigned long _nextScrollTime;
+
+    // true when the display is homing at least one character
+    bool _isHoming = false;
+
+    // true when the display motors are all disabled
+    bool _isDisabled = false;
+
+    // the output for the step pins that control motor movement (at last run)
+    byte _lastStepPinRegisterOutput = 0b00000000;
+
+    // the input for the button pins that detect motor loops
+    byte _buttonRegisterInput = 0b00000000;
+
+    /*************************************
+     * METHODS
+     *************************************/
+    /**
+     * @brief read in the GPIO values from the loop detection buttons
+     */
+    void readInButtonRegister();
+
+    /**
+     * @brief output the GPIO values
+     */
+    void updateRegisters();
+
+    /**
+     * @brief determine the new GPIO values for the output register
+     * @return byte
+     */
+    byte getRegisterOutput();
+
+    /**
+     * @brief determine if the display needs to scroll
+     */
+    void checkForScroll();
+
+    /**
+     * @brief determine if the display is currently homing
+     */
+    void checkForHoming();
+
+    /**
+     * @brief determine if the display needs to be enabled
+     */
+    void checkForEnable();
+
+    /**
+     * @brief determine if the display needs to be disabled
+     */
+    void checkForDisable();
+
+    /**
+     * @brief determine if all the characters are in the MOTOR_OFF state
+     * @return true when all motors are off
+     * @return false when any motors is on
+     */
+    bool areAllMotorsDisabled();
+
+    /**
+     * @brief start moving the characters
+     * Based on the animation type, set up a lerp for each character and kick
+     * them off
+     */
+    void lerpToCurrentDisplay();
 };
 
 #endif
